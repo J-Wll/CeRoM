751a5312a46ca121ad84d639e048fe4f8ef1c0cb
Joe-W <80954812+J-Wll@users.noreply.github.com>
2024-03-05 13:22:34 +0000

    Switched to pug

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const crypto = require("node:crypto");
+
+data = [{ name: crypto.randomUUID() }];
+
+for (let i = 0; i < 10; i++) {
+  data.push({ name: crypto.randomUUID() });
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express', data: data });
 });
 
 module.exports = router;
