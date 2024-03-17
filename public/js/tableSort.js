19b998832d192287a215dcd8d6fad09d560a68ad
Joe-W <80954812+J-Wll@users.noreply.github.com>
2024-03-17 15:38:44 +0000

    Added data table queries

--- public/js/tableSort.js
+++ public/js/tableSort.js
@@ -1,4 +1,7 @@
-
-function tableSort(e) {
+// universal sort for products, customers, etc
+// full table, index to sort by, str/num, asc/des
+function tableSort(data, field, direction = "asc") {
     console.log(e);
+    const sortedTable = data;
+    return sortedTable;
 }
