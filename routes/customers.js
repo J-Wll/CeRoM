bfa9cc370849bd18e5701eed3c018ee44a31dd90
Joe-W <80954812+J-Wll@users.noreply.github.com>
2024-03-18 13:04:53 +0000

    Changed customers.js to use getData function

--- routes/customers.js
+++ routes/customers.js
@@ -1,22 +1,16 @@
-// Basically just repeats product router, make more DRY with middleware
 var express = require('express');
 var router = express.Router();
-const fs = require("fs");
-const path = require("path");
 
-const tableSort = require("../public/js/tableSort")
-
-const file = fs.readFileSync(
-    path.resolve(__dirname, "../data/customer_data.json")
-);
-const customerData = JSON.parse(file);
+const getData = require("../middleware/getData")
 
 router.get("/", (req, res) => {
     const sortBy = req.query.sortBy || "ID";
     const sortDir = req.query.sortDir || "asc";
 
+    const productData = getData("../data/customer_data.json", sortBy, sortDir);
+
     res.render("products", {
-        data: tableSort(customerData, sortBy, sortDir),
+        data: productData,
         title: "Customers"
     })
 })
